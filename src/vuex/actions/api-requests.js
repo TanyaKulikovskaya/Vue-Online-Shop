2bd051e3f33b3b4cb987fb2ac051caf38a24166e
Tanya Kulikovskaya <tatsiana@pedanov.ru>
2021-08-24 17:24:46 +0300

    Added external json-api

--- src/vuex/actions/api-requests.js
+++ src/vuex/actions/api-requests.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const API_BASE_URL = 'https://my-json-server.typicode.com/TanyaKulikovskaya/Vue-Online-Shop'
+
 export default { // actions = methods
     GET_PRODUCTS_FROM_API({commit}) {
-        return axios.get('http://localhost:3000/products')
+        return axios.get(API_BASE_URL + '/products')
             .then((products) => {
                 commit('SET_PRODUCTS_TO_STATE', products.data);
                 return products;
@@ -13,7 +15,7 @@ export default { // actions = methods
             })
     },
     GET_PRODUCT_FROM_API({commit}, id) {
-        return axios.get(`http://localhost:3000/products/${id}`)
+        return axios.get(API_BASE_URL + '/products/' + `${id}`)
             .then((product) => {
                 commit('SET_PRODUCT_TO_STATE', product.data);
                 return product;
